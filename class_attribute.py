e95f767afbe47e54415f87956af63c85a8d72a63
Rakibhsaniu <rakibhsanoyoniu@gmail.com>
2023-10-04 10:26:23 +0600

    getter setter

---
+++ class_attribute.py
@@ -0,0 +1,7 @@
+class Shopping:
+    cart = [] #class attribute  # static attribute
+    origin = 'china'
+
+    def __init__(self, name,location):
+        self.name = 'Bogura' #instance attribute
+        self.location = 'jam er moddhe'
