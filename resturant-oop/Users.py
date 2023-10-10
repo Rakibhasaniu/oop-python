cc127863547c7222c57b2ba4a1319d9f83852eac
Rakibhsaniu <rakibhsanoyoniu@gmail.com>
2023-10-10 12:00:17 +0600

    resturanr oop started

---
+++ resturant-oop/Users.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+    def __init__(self,name) -> None:
+        self.name = name
+        
+class Customer(User):
+    def __init__(self, name,money) -> None:
+        self.wallet = money
+        self.__order = None
+        super().__init__(name)
+
+    @property
+    def order(self):
+        return self.__order
+    @order.setter
+    def order(self,order):
+        self.__order=order
