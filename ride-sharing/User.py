a1eef476e1219cfcd07138ae8b43bef573eb3406
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-10-15 23:51:27 +0600

    finished

--- ride-sharing/User.py
+++ ride-sharing/User.py
@@ -1,5 +1,24 @@
 from abc import ABC,abstractclassmethod
 from datetime import datetime
+
+
+
+class Ride_Sharing:
+    def __init__(self,company_name) -> None:
+        self.company_name = company_name
+        self.riders = []
+        self.drivers = []
+        self.rides = []
+
+    def add_rider(self, rider):
+        self.riders.append(rider)
+    def add_drivers(self, driver):
+        self.drivers.append(driver)
+
+    def __repr__(self) -> str:
+        
+        return f'{self.company_name} with riders : {len(self.riders)} and driver: {len(self.drivers)} '
+
 class User(ABC):
     def __init__(self,name,email,nid) -> None:
         self.name = name
@@ -14,9 +33,10 @@ class User(ABC):
         raise NotImplementedError
     
 class Rider(User):
-    def __init__(self, name, email, nid,current_location) -> None:
+    def __init__(self, name, email, nid,current_location,initial_amount) -> None:
         self.current_ride = None
-        self.wallet = 0
+        self.wallet = initial_amount
+        self.current_location = current_location
         super().__init__(name, email, nid)
 
     def display_profile(self):
@@ -28,12 +48,16 @@ class Rider(User):
     def update_location (self,current_location):
         self.current_location = current_location
 
-    def request_ride(self,location,destination):
+    def request_ride(self,ride_sharing,destination):
         if not self.current_ride:
             #TODO: set ride properly
             #TODO: set current ride via ride match
-            ride_request = None
-            self.current_ride = None
+            ride_request = Ride_request(self,destination)
+            ride_matcher = Ride_Matching(ride_sharing.drivers)
+            self.current_ride = ride_matcher.find_driver(ride_request)
+
+    def show_current_ride(self):
+        print(self.current_ride)
 
 class Driver(User):
     def __init__(self, name, email, nid,current_location) -> None:
@@ -66,7 +90,8 @@ class Ride:
         self.end_time = datetime.now()
         self.rider.wallet  -= self.estimated_fare
         self.driver.wallet += self.estimated_fare
-
+    def __repr__(self) -> str:
+        return f'Ride Details:Started: {self.start_location} to {self.end_location} '
 
 class Ride_request:
     def __init__(self, rider, end_location, destination) -> None:
@@ -75,11 +100,60 @@ class Ride_request:
 
 
 class Ride_Matching:
-    def __init__(self) -> None:
-        self.available_drivers = []
+    def __init__(self,drivers) -> None:
+        self.available_drivers = drivers
     
     def find_driver(self,ride_request):
         if len(self.available_drivers) > 0:
             #TODO: find the closest driver of the rider
             driver = self.available_drivers[0]
             ride = Ride(ride_request.rider.current_location, ride_request.rider.end_location )
+            driver.accept_ride(ride)
+            return ride
+
+class Vehicle(ABC):
+
+    speed = {
+        'car': 50,
+        'bike': 60,
+        'cng': 15
+    }
+
+
+    def __init__(self,vehicle_type,license_plate,rate) -> None:
+        self.vehicle_type = vehicle_type
+        self.license_plate = license_plate
+        self.rate = rate
+        self.status = 'available'
+
+
+    @abstractclassmethod 
+    def start_driving(self):
+        pass
+
+class Car(Vehicle):
+    def __init__(self, vehicle_type, license_plate, rate) -> None:
+        super().__init__(vehicle_type, license_plate, rate)
+
+    def start_driving(self):
+        self.status = 'unavailable'
+
+class Bike(Vehicle):
+    def __init__(self, vehicle_type, license_plate, rate) -> None:
+        super().__init__(vehicle_type, license_plate, rate)
+
+    def start_driving(self):
+        self.status = 'unavailable'
+
+
+#check the class integration
+
+niye_jao = Ride_Sharing('Niye_jao')
+# print("Riders : ", niye_jao._riders)
+rakib = Rider('Rakib','rakib@gmail.com',5457,'natore',1500)
+niye_jao.add_rider(rakib)
+sohana = Driver('sohana','sohana@gmailcom',678,'singra')
+niye_jao.add_drivers(sohana)
+print(niye_jao)
+rakib.request_ride(niye_jao,'bogura')
+rakib.show_current_ride()
