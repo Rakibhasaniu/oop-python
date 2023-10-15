4e41561c1ead2d8dc767a5dcf58484a04503e062
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-10-15 20:33:33 +0600

    User and Rider class created

--- ride-sharing/User.py
+++ ride-sharing/User.py
@@ -1,7 +1,28 @@
 from abc import ABC,abstractclassmethod
 class User(ABC):
-    def __init__(self,name,email) -> None:
+    def __init__(self,name,email,nid) -> None:
         self.name = name
         self.email = email
         #TODO: set user id dynamically
         self.__id = 0
+        self.__nid = nid
+        self.wallet = 0
+
+    @abstractclassmethod
+    def display_profile(self):
+        raise NotImplementedError
+    
+class Rider(User):
+    def __init__(self, name, email, nid) -> None:
+        super().__init__(name, email, nid)
+
+    def display_profile(self):
+        print(f'Rider: with name {self.name} and email: {self.email}')
+
+    def request_ride(self,location,destination):
+        if not self.current_ride:
+            #TODO: set ride properly
+            #TODO: set current ride via ride match
+            ride_request = None
+            self.current_ride = None
+
