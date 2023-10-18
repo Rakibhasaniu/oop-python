6a0ceae3dcfda7be8b7cda27e5d896fd46d9f355
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-10-18 21:35:53 +0600

    new changes

--- object.py
+++ object.py
@@ -2,6 +2,8 @@ class Person:
     def __init__(self, name,age):
         self.name = name 
         self.age = age
+        print(name)
+        print(age)
        
 
     def get_name(self):
@@ -9,7 +11,7 @@ class Person:
     def get_age(self):
         return self.age
     
-a_person = Person("Rakib Hasan",45)
+a_person = Person("Rakib Hasan",5)
 b_person = Person("Sohana Shitol",45)
 print(a_person.get_name())
 print(b_person.get_name())
